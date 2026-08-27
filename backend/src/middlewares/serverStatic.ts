4564d8b877d064c30b8b9842785938c26df8e590
regina nightray <regi1312@gmail.com>
2026-08-27 15:49:06 +0300

    fix: prevent path traversal in static fie middleware

--- backend/src/middlewares/serverStatic.ts
+++ backend/src/middlewares/serverStatic.ts
@@ -5,16 +5,24 @@ import path from 'path'
 export default function serveStatic(baseDir: string) {
     return (req: Request, res: Response, next: NextFunction) => {
         // Определяем полный путь к запрашиваемому файлу
-        const filePath = path.join(baseDir, req.path)
+        const resolvedBase = path.resolve(baseDir);
+        const filePath = path.join(baseDir, req.path);
+        const resolvedPath = path.resolve(filePath);
 
+        if (
+            resolvedPath !== resolvedBase &&
+            !resolvedPath.startsWith(resolvedBase + path.sep)
+        ) {
+            return next();
+        }
         // Проверяем, существует ли файл
-        fs.access(filePath, fs.constants.F_OK, (err) => {
+        fs.access(resolvedPath, fs.constants.F_OK, (err) => {
             if (err) {
                 // Файл не существует отдаем дальше мидлварам
                 return next()
             }
             // Файл существует, отправляем его клиенту
-            return res.sendFile(filePath, (err) => {
+            return res.sendFile(resolvedPath, (err) => {
                 if (err) {
                     next(err)
                 }
